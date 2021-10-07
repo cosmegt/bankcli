2b19e52f9c34f0738e0dc56662b73c3bcfe9a13d
Ubuntu <cosme>
2021-10-07 19:36:41 +0000

    added header and welcome_mesage()

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
-#include<stdio.h>
+/**
+ * BankCLI main.c
+ * We attempt to make a banking system.
+ * The system should be able to deposit, withdrawal, and check balance.
+ * The banking system has a user system with a pin and user_id to do operations
+ * That's it for now.
+ *
+ * This is just C language practice.
+ *
+ * Author: Cosme Chavez
+ * */
+
+
+#include <stdio.h>
+
+
+
+int main(int argc[], char *argv[]){
+	welcome_message();
+}
+
+
+void welcome_message(){
+	println("Welcome to ATM CLI\n Please enter your username: ")
+}
